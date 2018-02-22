93c033c0b5e85653042fcb5e4156db2847610b89
Randy Wardhana <randywardhana37@gmail.com>
2018-02-22 14:44:55 +0700

    Update Front-End

--- RELIST/source/RootPage.js
+++ RELIST/source/RootPage.js
@@ -14,7 +14,7 @@ import {Actions} from 'react-native-router-flux';
 import Index from './login/Index';
 import Active from './interface/Active';
 import Apidemo from '../utilities/api/Apidemo';
-import { Container, Content, Header, Body, Icon, List, Button } from 'native-base';
+import { Container, Content, Header, Body, Icon, List, Button, Footer, FooterTab } from 'native-base';
 import { StackNavigator, NavigationAction, TabNavigator,DrawerNavigator, DrawerItems } from 'react-navigation';
 
 export default class RootPage extends Component{
@@ -33,23 +33,33 @@ export default class RootPage extends Component{
 
 	render(){
 		return(
-		<Content style={{backgroundColor:'#ffffff'}}>
-			<List>	
-				<Header androidStatusBarColor="rgb(46,56,58)" style={{ height: 200, backgroundColor: 'rgb(46,56,58)'}}>
-					<Body>
-						<Image style={styles.drawerImage} source={require('./image/icon.png')} />
-					</Body>
-				</Header>
-				<Button transparent dark onPress={() => this.login()} >
-					<Icon name="md-log-out" style={{marginLeft: 10, fontSize: 30}} />
-						<Text style={styles.itemExit}>Logout</Text>
-				</Button>
-				<Button transparent dark onPress={() => this.exit_function()} >
-					<Icon name="md-power" style={{marginLeft: 10, fontSize: 30}} />
-						<Text style={styles.itemExit}>Exit</Text>
-				</Button>                
-			</List>
-		</Content>
+			<Container>
+				<Content style={{backgroundColor:'#ffffff'}}>
+					<List>	
+						<Header androidStatusBarColor="rgb(46,56,58)" style={{ height: 200, backgroundColor: 'rgb(46,56,58)'}}>
+							<Body style={{alignItems: 'center', justifyContent: 'center',}}>
+								<Image style={styles.drawerImage} source={require('./image/icon.png')} />
+							</Body>
+						</Header>
+						<Button active transparent dark style={styles.itemExit} onPress={() => this.index()} >
+							<Icon active name="md-home" />
+								<Text style={{color: 'black'}}>Home</Text>
+						</Button>
+						<Button active transparent dark style={styles.itemExit} onPress={() => this.exit_function()} >
+							<Icon active name="md-person" />
+								<Text style={{color: 'black'}}>Profile</Text>
+						</Button>   
+					</List>
+				</Content>
+				<Footer>
+					<FooterTab>
+						<Button full active transparent light onPress={() => this.login()} style={{backgroundColor: 'rgb(46,56,58)'}}>
+							<Icon name="md-log-out" />
+								<Text style={{color: 'white'}}>Logout</Text>
+						</Button>
+					</FooterTab>
+				</Footer>
+			</Container>
 		);
 	}
 }
@@ -57,15 +67,11 @@ export default class RootPage extends Component{
 
 styles = StyleSheet.create({
 	drawerImage: {
-		alignItems: 'center',
-		justifyContent: 'center',
-		marginLeft: 75,
 		height:130,
 		width:130
 	},
     itemExit: {
     	left:0,
 		bottom:0,
-		color: 'black',
     },
 })
